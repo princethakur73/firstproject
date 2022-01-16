c83455b6f1048d13eab65861cd60b51a5175ff08
MSPL Ved MSPL Ent <msplitservices@hotmail.com>
2022-01-16 12:44:40 +0530

    16 Jan 2022

--- WebApplication/WebApplication/Scripts/Circulars/Circulars.js
+++ WebApplication/WebApplication/Scripts/Circulars/Circulars.js
@@ -20,7 +20,7 @@ $(document).on('click', 'table > tbody > tr > td > .pull-right > .delete', funct
     if (a) {
         var $buttonDelete = $(this);
         var id = $(this).attr('data-id');
-        $.get('/Admin/TransferCertificate/delete', { "Id": id }, function (data) {
+        $.get('/Admin/Circulars/delete', { "Id": id }, function (data) {
             if (data === true) {
                 $buttonDelete.closest('.row-command').remove();
                 $('#alert').html("<div class='container-fluid alert-container'><div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Record deleted successfully.</div></div>");
@@ -40,7 +40,7 @@ $(document).on('click', 'li > a', function () {
     var templateHtml = $('#table-list').html();
     var templateCompile = Handlebars.compile(templateHtml);
 
-    $.get('/Admin/TransferCertificate/GetTransferCertificateList', { 'pageNumber': parseInt(pageNo), 'pageSize': 10 }, function (data) {
+    $.get('/Admin/Circulars/GetCircularsList', { 'pageNumber': parseInt(pageNo), 'pageSize': 10 }, function (data) {
         var templateResult = templateCompile(data);
         $('#table').html(templateResult);
     });
