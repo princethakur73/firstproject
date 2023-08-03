95b0ffc150d48a3fd888c74c212a8246d13e9ec1
Prince Kumar <princethakur73@gmail.cin>
2023-08-04 00:33:29 +0530

    Validation on recruitment form

--- WebApplication/WebApplication.Core/Model/RecruitmentModel.cs
+++ WebApplication/WebApplication.Core/Model/RecruitmentModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Web;
 
 namespace WebApplication.Core.Model
@@ -11,20 +6,27 @@ namespace WebApplication.Core.Model
     public class RecruitmentModel
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Name Required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string FullName { get; set; }
         [Required]
         public string Gender { get; set; }
-        [Required(ErrorMessage ="DOB Required")]
+        [Required(ErrorMessage = "DOB Required")]
         public string DOB { get; set; }
         [Required]
         public string Address { get; set; }
-        [EmailAddress(ErrorMessage ="Invalid email address")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         [Required(ErrorMessage = "Email Required")]
         public string Email { get; set; }
+
+        [MaxLength(12)]
+        [MinLength(10)]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Contact must be numeric")]
         [Required(ErrorMessage = "Contact Required")]
         public string Contact { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string ApplyFor { get; set; }
         [Required(ErrorMessage = "Resume/CV Required")]
         public HttpPostedFileBase RecruitmentFile { get; set; }
